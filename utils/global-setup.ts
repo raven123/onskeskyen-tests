ec290234ae4ad1ac84748380c955535181a62007
raven123 <ravirai121@gmail.com>
2025-06-02 23:02:37 +0200

    updated to headless mode

--- utils/global-setup.ts
+++ utils/global-setup.ts
@@ -6,11 +6,10 @@ import path from 'path';
 import { env } from 'process';
 
 const envPath = path.resolve(__dirname, '../env/local.env');
-console.log(`Loading environment variables from: ${envPath}`);
 dotenv.config({ path: path.resolve(__dirname, envPath) });
 
 export default async function globalSetup() {
-  const browser = await chromium.launch({headless: false});
+  const browser = await chromium.launch({headless: true});
   const context = await browser.newContext();
 
   await loginWithUser(context,process.env.USER1_EMAIL!, process.env.USER1_PASSWORD!);
